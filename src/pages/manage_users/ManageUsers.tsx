d9c3caacb7bec9ee545704218f8bbe21810ca51f
jubayer <jubayerahmed.dev@gmail.com>
2025-08-13 12:24:39 +0600

    Fetched users and showed on admin dashbord

--- src/pages/manage_users/ManageUsers.tsx
+++ src/pages/manage_users/ManageUsers.tsx
@@ -3,59 +3,13 @@ import { useEffect, useState } from "react";
 import SearchBox from "./components/SearchBox";
 import Table from "./components/Table";
 
-const initialData = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john.doe@example.com",
-    role: "Developer",
-    status: "Active",
-  },
-  {
-    id: 2,
-    name: "Jane Smith",
-    email: "jane.smith@example.com",
-    role: "Designer",
-    status: "Inactive",
-  },
-  {
-    id: 3,
-    name: "Peter Jones",
-    email: "peter.jones@example.com",
-    role: "Manager",
-    status: "Active",
-  },
-  {
-    id: 4,
-    name: "Mary Johnson",
-    email: "mary.j@example.com",
-    role: "QA",
-    status: "Active",
-  },
-  {
-    id: 5,
-    name: "David Brown",
-    email: "david.b@example.com",
-    role: "Developer",
-    status: "Inactive",
-  },
-  {
-    id: 6,
-    name: "Susan Davis",
-    email: "susan.d@example.com",
-    role: "Product Manager",
-    status: "Active",
-  },
-];
-
 export default function ManageUsers() {
-  const [data, setData] = useState(initialData);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const getUsers = async () => {
       const response = await axios(
         `${import.meta.env.VITE_BACKEND_URL}/api/v1/users`,
-        // {},
         {
           headers: {
             Authorization:
@@ -64,11 +18,13 @@ export default function ManageUsers() {
           },
         }
       );
-      console.log(response);
+      setUsers(response.data.data.users);
     };
 
     getUsers();
   }, []);
+
+  console.log(users);
 
   return (
     <div className="min-h-screen  p-2">
@@ -76,7 +32,7 @@ export default function ManageUsers() {
 
       <div className="bg-white shadow-sm rounded-xl overflow-hidden">
         <div className="overflow-x-auto">
-          <Table data={data} />
+          <Table users={users} />
         </div>
       </div>
     </div>
--- src/pages/manage_users/components/Table.tsx
+++ src/pages/manage_users/components/Table.tsx
@@ -1,101 +1,79 @@
-const StatusBadge = ({ status }) => {
-  const color =
-    status === "Active"
-      ? "bg-green-100 text-green-800"
-      : "bg-gray-100 text-gray-800";
-  return (
-    <span
-      className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${color}`}
-    >
-      {status}
-    </span>
-  );
-};
-
 interface DataProps {
-  id: number;
-  name: string;
-  email: string;
-  role: string;
-  status: string;
+  _id: string;
+  username: string;
+  wallet: object;
+  cratedAt: Date;
 }
 
-function Table({ data }: { data: DataProps[] }) {
+const tableColumns = [
+  "Username",
+  "Main Balance",
+  "Total Withdrawn",
+  "Referral Income",
+  "Created At",
+];
+
+function Table({ users }: { data: DataProps[] }) {
   return (
     <table className="min-w-full divide-y divide-gray-200">
       <thead className="bg-gray-50">
         <tr>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-          >
-            Name
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-          >
-            Email
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-          >
-            Role
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-          >
-            Status
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-          >
-            Actions
-          </th>
+          {tableColumns.map((col) => {
+            return <Th label={col} />;
+          })}
         </tr>
       </thead>
       <tbody className="bg-white divide-y divide-gray-200">
-        {data.map((item) => (
-          <tr
-            key={item.id}
-            className="hover:bg-gray-50 transition duration-150 ease-in-out"
-          >
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="flex items-center">
-                <div className="ml-4">
-                  <div className="text-sm font-medium text-gray-900">
-                    {item.name}
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="text-sm text-gray-900">{item.email}</div>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="text-sm text-gray-900">{item.role}</div>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <StatusBadge status={item.status} />
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-              <a
-                href="#"
-                className="text-indigo-600 hover:text-indigo-900 mr-4"
-              >
-                Edit
-              </a>
-              <a href="#" className="text-red-600 hover:text-red-900">
-                Delete
-              </a>
-            </td>
-          </tr>
+        {users.map((user) => (
+          <Tr user={user} />
         ))}
       </tbody>
     </table>
   );
 }
 export default Table;
+
+const Tr = ({ user }) => {
+  const createData = user.createdAt.slice(0, 10);
+  return (
+    <tr
+      key={user._id}
+      className="hover:bg-gray-50 transition duration-150 ease-in-out"
+    >
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="flex items-center">
+          <div className="ml-4">
+            <div className="text-sm font-medium text-gray-900">
+              {user.username}
+            </div>
+          </div>
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-900">{user.wallet.mainBalance}</div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-900">
+          {user.wallet.totalWithdrawn}
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        {user.wallet.referralIncome}
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
+        {createData}
+      </td>
+    </tr>
+  );
+};
+
+const Th = ({ label }) => {
+  return (
+    <th
+      scope="col"
+      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+    >
+      {label}
+    </th>
+  );
+};
